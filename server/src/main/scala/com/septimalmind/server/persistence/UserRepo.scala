a3346b3a67fe15a43b4ef2d1ba2f8f06f05c5a3a
ratoshniuk <mratoshniuk@gmail.com>
2019-05-07 12:08:18 +0300

    aqcuire id for pending confirmation of login

--- server/src/main/scala/com/septimalmind/server/persistence/UserRepo.scala
+++ server/src/main/scala/com/septimalmind/server/persistence/UserRepo.scala
@@ -4,6 +4,7 @@ import com.github.pshirshov.izumi.functional.bio.BIO
 import com.github.pshirshov.izumi.functional.bio.BIO._
 import com.septimalmind.services.shared.{DomainFailure, DomainFailureCode}
 import com.septimalmind.services.users.{UserId, UserProfile}
+import com.septimalmind.services.auth.TwoFactorAuthFormat
 
 class UserRepo[F[+_, +_]: BIO] {
 
@@ -11,6 +12,8 @@ class UserRepo[F[+_, +_]: BIO] {
   import bio._
 
   private val usersStorage = scala.collection.mutable.HashMap.empty[UserId, UserProfile]
+
+  private val authPreferencesStorage = scala.collection.mutable.HashMap.empty[UserId, TwoFactorAuthFormat]
 
   def registerUser(userProfile: UserProfile) : F[DomainFailure, Unit] = {
     point(usersStorage.put(userProfile.id, userProfile))
@@ -20,7 +23,26 @@ class UserRepo[F[+_, +_]: BIO] {
     point(usersStorage.remove(userId)).void
   }
 
-  def findByEmail(email: String) : F[DomainFailure, UserId] = {
-    fromOption(DomainFailure(DomainFailureCode.EntityNotFound, Some("user with email not found")))(usersStorage.find(_._2.email == email).map(_._1))
+  def checkPw(userId: UserId, pw: String) : F[DomainFailure, Unit] = {
+    for {
+      curPw <- fromOption(DomainFailure(DomainFailureCode.EntityNotFound, Some("user with email not found")))(usersStorage.get(userId)).map(_.password)
+      _ <- when(curPw != pw)(fail(DomainFailure(DomainFailureCode.AssertionFailed, Some("wrong password for user"))))
+    } yield ()
+  }
+
+  def findByEmail(email: String) : F[DomainFailure, UserProfile] = {
+    fromOption(DomainFailure(DomainFailureCode.EntityNotFound, Some("user with email not found")))(usersStorage.find(_._2.email == email).map(_._2))
+  }
+
+  def findByPhone(phone: String) : F[DomainFailure, UserProfile] = {
+    fromOption(DomainFailure(DomainFailureCode.EntityNotFound, Some("user with phone not found")))(usersStorage.find(_._2.phone == phone).map(_._2))
+  }
+  
+  def upsertAuthFormat(userId: UserId, format: TwoFactorAuthFormat) : F[Nothing, Unit] = {
+    point(authPreferencesStorage.put(userId, format))
+  }
+
+  def retrieveUserAuthFormat(userId: UserId) : F[Nothing, TwoFactorAuthFormat] = {
+    point(authPreferencesStorage.getOrElseUpdate(userId, TwoFactorAuthFormat.None))
   }
 }
--- server/src/main/scala/com/septimalmind/server/services/auth/LoginService.scala
+++ server/src/main/scala/com/septimalmind/server/services/auth/LoginService.scala
@@ -7,16 +7,15 @@ import com.github.pshirshov.izumi.functional.bio.BIO._
 import com.github.pshirshov.izumi.logstage.api.IzLogger
 import com.septimalmind.server.externals.TokenService
 import com.septimalmind.server.idl.RequestContext
-import com.septimalmind.server.idl.RequestContext.{AdminRequest, ClientRequest}
-import com.septimalmind.server.persistence.{UserRepo, UserSessionRepo}
+import com.septimalmind.server.idl.RequestContext.{ AdminRequest, ClientRequest }
+import com.septimalmind.server.persistence.{ UserRepo, UserSessionRepo }
 import com.septimalmind.services.auth._
-import com.septimalmind.services.shared.{DomainFailure, _}
-import com.septimalmind.services.users.{ProtectedProfile, PublicProfile, UserId, UserProfile}
+import com.septimalmind.services.shared._
+import com.septimalmind.services.users._
 
 import scala.language.higherKinds
 
-class LoginService[F[+ _, + _] : BIO]
-(
+class LoginService[F[+ _, + _]: BIO](
   logger: IzLogger,
   tokenService: TokenService[F],
   userRepo: UserRepo[F],
@@ -27,52 +26,61 @@ class LoginService[F[+ _, + _] : BIO]
 
   import bio._
 
-  override def login
-  (
+  override def login(
     ctx: RequestContext,
     creds: Credentials
   ): F[DomainFailure, LoginResponse] = {
 
     logger.info(s"start processing ${creds -> "tokens"}")
 
-    creds match {
-      case Credentials.EmailPassword(emailPw) =>
-        for {
-          userId <- userRepo.findByEmail(emailPw.email)
-          token <- generateToken(userId, ctx.networkContext.ip)
-        } yield AccessResponse(token)
-      case Credentials.PhonePassword(_) =>
-        //TODO: implement
-        throw new IllegalArgumentException("Not implemented")
-    }
+    for {
+      (user, pw) <- creds match {
+                     case Credentials.EmailPassword(emailPw) =>
+                       userRepo.findByEmail(emailPw.email).map(_ -> emailPw.password)
+                     case Credentials.PhonePassword(phonePw) =>
+                       userRepo.findByPhone(phonePw.phone).map(_ -> phonePw.password)
+                   }
+      id     = user.id
+      ip     = ctx.networkContext.ip
+      _      <- userRepo.checkPw(id, pw)
+      format <- userRepo.retrieveUserAuthFormat(id)
+      out <- {
+        import LoginResponse._
+        format match {
+          case TwoFactorAuthFormat.None =>
+            generateToken(id, ip).map(LoginResponse.intoAccessResponse)
+          case enabled =>
+            twoFactorAuthenticate(user.id, enabled, ip).map(LoginResponse.intoPendingConfirmation)
+        }
+      }
+    } yield out
   }
 
-  override def signUp
-  (
+  override def signUp(
     ctx: RequestContext,
     request: SignUpRequest
-  ): F[DomainFailure, AccessResponse] = {
-
+  ): F[DomainFailure, AccessResponse] =
     ctx match {
       case AdminRequest(companyId, networkContext) =>
         val userId = UserId(UUID.randomUUID(), companyId.value)
-        val user = UserProfile.apply(request.to[PublicProfile], request.to[ProtectedProfile], userId)
-        userRepo.registerUser(user)
-        generateToken(userId, networkContext.ip).map(AccessResponse(_))
+        val user   = UserProfile.apply(request.to[PublicProfile], request.to[ProtectedProfile], userId)
+        for {
+          _     <- userRepo.registerUser(user)
+          _     <- userRepo.upsertAuthFormat(userId, request.authFormat)
+          token <- generateToken(userId, networkContext.ip)
+        } yield AccessResponse(token)
       case _ =>
         fail(DomainFailure(DomainFailureCode.AccessDenied, Some("method allowed only for administrators")))
     }
-  }
 
   def completeLogin(ctx: RequestContext, token: String): F[DomainFailure, AccessResponse] = {
-    ???
+    
   }
+    
 
-  override def logout
-  (
+  override def logout(
     ctx: RequestContext
-  ): F[DomainFailure, SuccessResponse] = {
-
+  ): F[DomainFailure, SuccessResponse] =
     ctx match {
       case ClientRequest(userId, networkContext) =>
         for {
@@ -82,24 +90,27 @@ class LoginService[F[+ _, + _] : BIO]
       case _ =>
         point(SuccessResponse(None))
     }
-  }
 
-  protected def generateToken(userId: UserId, ip: String): F[DomainFailure, String] = {
+  protected def twoFactorAuthenticate(
+    userId: UserId,
+    format: TwoFactorAuthFormat,
+    ip: String
+  ): F[DomainFailure, PendingConfirmation] = ???
+
+  protected def generateToken(userId: UserId, ip: String): F[DomainFailure, AccessResponse] =
     for {
-      _ <- point(logger.apply("user" -> userId, "ip" -> ip).info("generating access token"))
+      _     <- point(logger.apply("user" -> userId, "ip" -> ip).info("generating access token"))
       token <- tokenService.generateToken(userId)
-      _ <- sessionRepo.register(userId, ip, token)
-    } yield token
-  }
+      _     <- sessionRepo.register(userId, ip, token)
+    } yield AccessResponse(token)
 
-  private def checkoutSession(userId: UserId, ip: String): F[DomainFailure, Unit] = {
+  private def checkoutSession(userId: UserId, ip: String): F[DomainFailure, Unit] =
     sessionRepo.fetchSessionId(userId, ip).void.leftMap {
-      case e@DomainFailure(DomainFailureCode.EntityNotFound, msg) =>
+      case e @ DomainFailure(DomainFailureCode.EntityNotFound, msg) =>
         logger.apply("user" -> userId, "ip" -> ip).info("can't find pair. reject")
         e.copy(code = DomainFailureCode.AccessDenied)
       case other =>
         other
     }
-  }
 
 }
